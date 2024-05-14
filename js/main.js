06461dd492fe25881e7ebaaf98ddc7a3ff2a56e8
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-14 10:22:31 +0800

    Add another way to solve the problem 3 for rock-paper-scissors.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // *Variables*
-// Declare a variable and assign it to your fav drink as a string. Make sure there is no whitespace on either side of the string, and print the value to the console
+//1. Declare a variable and assign it to your fav drink as a string. Make sure there is no whitespace on either side of the string, and print the value to the console
 
 let favDrink = ' Coke ' ;
 favDrink = favDrink.trim();
@@ -15,7 +15,7 @@ if (fruits.search('apple') !== -1) {
 
 
 // *Functions*
-// Create a function that returns rock, paper, or scissors as randomly as possible
+//2. Create a function that returns rock, paper, or scissors as randomly as possible
 // let tool = ['rock', 'paper', 'scissors'];
 
 // function selectRandomTool() {
@@ -27,11 +27,28 @@ if (fruits.search('apple') !== -1) {
 
 // selectRandomTool();
 
-// *Conditionals*
-//Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
-function meVsBot () {
-  let selectTool = document.querySelector('#').value
+// Another way to solve it:
+let tool = ['rock', 'paper', 'scissors'];
+
+function selectRandomTool() {
+  let randomIndex = Math.random();
+  if (randomIndex < 0.33) {
+    console.log('Rock');
+  }else if (randomIndex < 0.66) {
+    console.log('Scissors');
+  }else {
+    console.log('Paper');
+  }
 }
+selectRandomTool()
+selectRandomTool()
+selectRandomTool()
+
+// *Conditionals*
+//3. Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
+// function meVsBot () {
+//   let selectTool = document.querySelector('#').value
+// }
 
 //*Loops*
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
