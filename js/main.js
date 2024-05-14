48e924c694cb8f1e920241135aadf268bb650cca
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-14 09:48:06 +0800

    First commit.

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// *Variables*
+// Declare a variable and assign it to your fav drink as a string. Make sure there is no whitespace on either side of the string, and print the value to the console
+
+let favDrink = ' Coke ' ;
+favDrink = favDrink.trim();
+console.log(favDrink);
+
+// //Declare a variable, assign it a string of multiple words, and check to see if one of the words is "apple".
+let fruits = "Eve gave apple to Adam."
+console.log(fruits.search('apple'));
+
+
+// *Functions*
+// Create a function that returns rock, paper, or scissors as randomly as possible
+// let tool = ['rock', 'paper', 'scissors'];
+
+// function selectRandomTool() {
+//   const min = 0;
+//   const max = tool.length;
+//   const randomIndex = Math.floor(Math.random() * (max - min)) + min;
+//   console.log (tool[randomIndex]);
+// }
+
+// selectRandomTool();
+
+// *Conditionals*
+//Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
+function meVsBot () {
+  let selectTool = document.querySelector('#').value
+}
+
+//*Loops*
+//Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
